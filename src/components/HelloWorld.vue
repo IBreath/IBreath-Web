55c5530723aa3835b9c0184a3e81dbf30730b360
Corentin RICHARD <corentin.richard@lephare.com>
2018-06-14 14:30:03 +0200

    Coucou

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,21 +2,65 @@
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
+        <line-chart :width="1000"
+                    :height="200"
+                    :chart-data="datacollection">
+
+        </line-chart>
+        <button @click="fillData()">Refresh</button>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
 </template>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<script>
+    import LineChart from './LineChart'
+    import axios from 'axios';
+
+    export default {
+        components: {
+            LineChart
+        },
+        data () {
+            return {
+                datacollection: null,
+                measures : [],
+                dates: [],
+                data : [],
+            }
+        },
+        created () {
+            axios.get(`/api/measures/10/1`, {})
+            .then(response => {
+                this.data = response.data;
+                this.measures = response.data.map(a => a.value);
+                this.dates = response.data.map(a => { return a.dateTime[0] + '/' + a.dateTime[1] + '/' + a.dateTime[2];});
+                this.fillData();
+            })
+            .catch(e => {
+                this.errors.push(e)
+            })
+        },
+        mounted () {
+            this.fillData()
+        },
+        methods: {
+            fillData () {
+                this.datacollection = {
+                    labels: this.dates,
+                    datasets: [
+                        {
+                            label: 'Mesures',
+                            backgroundColor: '#f87979',
+                            data: this.measures,
+                        }
+                    ]
+                }
+            },
+        }
+    }
+</script>
+
 <style scoped>
 h1, h2 {
   font-weight: normal;
